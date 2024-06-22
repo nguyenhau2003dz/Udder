d08d57259902bf8943fe5bc06a95e5a37c36f9ea
nguyenhau2003dz <nguyenduchaudeptrai2003@gmail.com>
2024-06-22 16:47:37 +0700

    add viewWillAppear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        //
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
 
 }
 
